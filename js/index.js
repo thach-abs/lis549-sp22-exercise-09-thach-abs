686650852efb60bc1cc72fa940a3929a16609fb9
abbie <athach@uw.edu>
2022-05-27 15:22:41 -0700

    set up

---
+++ js/index.js
@@ -0,0 +1,58 @@
+//Log out a message to show you've got JavaScript running
+
+
+
+
+//Define a variable `myName` and log it out
+
+
+
+
+//Define a function `getVowelCount()`
+
+
+
+
+//Define a variable `numVowelsInName` and log it out
+
+
+
+
+//Create a variable `h1Elememt` that refers to the `<h1>` element in the DOM.
+//Then change the text of that element
+
+
+
+
+//Create a variable `footerElement` that refers to the `<footer>`, then
+//change the HTML content of that element
+
+//hints
+
+
+//Change the CSS `display` property of the `#cats` element
+//Add the `active` class to the `#btnShowDogs` element
+
+
+
+
+//Add an event listener to the buttons to respond to click events.
+//The listener's function will toggle the `#dogs` and `#cats` divs,
+//and toggle which button has the `active` class
+
+
+
+
+//Change the `cursor` CSS property of the images
+
+
+
+
+//Add the `data-bs-toggle` and `data-bs-target` attributes to the images
+
+
+
+
+//Ad an event listener to the modal for `show.bs.modal` events.
+//The listener's function will replace the modal's image `src` and
+//`alt` attributes with the values from the clicked image.
